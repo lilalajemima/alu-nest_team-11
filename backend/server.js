93b33bcf21760906385a48bd080931429d834dbc
Emma-Asoliya <e.briggs@alustudent.com>
2025-03-16 20:02:01 +0200

    connection successfula and secured

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+console.log("MongoDB URI: ", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+// Test route to verify database connection
+app.get("/test", (req, res) => {
+    mongoose.connection.db.listCollections()
+        .toArray((err, collections) => {
+            if (err) {
+                res.status(500).send('Error listing collections');
+            } else {
+                res.status(200).json(collections);
+            }
+        });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
