34d09fd12ccfcb4b61a137893580ff1360dcfaad
Winston Nji <winston00russel@gmail.com>
2025-03-16 15:10:57 +0200

    Removed all redundant files from repo

--- Homepage/script.js
+++
@@ -1,5 +0,0 @@
-// Homepage-specific JavaScript
-console.log("Homepage script loaded");
-
-// Example: Add any homepage-specific functionality here
-// For example, you could add animations or dynamic content updates
